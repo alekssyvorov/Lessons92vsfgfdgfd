bd74e97d58bff4f13d6e7bf7787efc8732f65274
alekssyvorov <123986551+alekssyvorov@users.noreply.github.com>
2023-10-28 06:29:56 +0000

    vsvsdvsv

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-a = int(input('A ')) # 2
-b = int(input("B ")) # 10
- # 3 4 5 6 7 8 9
-while a < b-1:
-    a = a + 1
-    print(a, end=" ")
-    
+num = int(input('N '))
+s = input('S ')
 
-
-
+while num > 0:
+    print(s)
+    num -= 1
